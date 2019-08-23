d775e88402b1f86221f97106fb50fda26730054e
thecodestuff <mahendrachoudhary1156@gmail.com>
2019-08-23 18:29:43 +0530

    minor tweaks

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,12 @@
 Rails.application.routes.draw do
   root to: 'home#index'
-  devise_for :users
+  #devise_for :users
+  devise_for :users, :controllers => {:registrations => 'admin/registrations'}
+
   devise_scope :user do
     get 'login', to: 'devise/sessions#new'
   end
+
   namespace :admin do
     #get '', to: 'admin#index'
     root to: 'admin#index'
