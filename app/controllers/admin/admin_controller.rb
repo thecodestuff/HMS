9cad08134ac078d90db42f24345b10c73e206810
thecodestuff <mahendrachoudhary1156@gmail.com>
2019-08-23 11:25:40 +0530

    patched get_users_count added to before_action rather calling in ac tion

--- app/controllers/admin/admin_controller.rb
+++ app/controllers/admin/admin_controller.rb
@@ -1,18 +1,16 @@
 class Admin::AdminController < ApplicationController
-  before_action :authenticate_user!
+  before_action :authenticate_user!, :get_users_count
   def index
-    #if user_signed_in? and !current_user.admin?
-      #render html: "hello guest"
-    #end
-    get_users_count
+    
   end
 
   protected
+
   def get_users_count
-    @users_count  = User.all 
-    @admin_count  = @users_count.where(admin:true).count
-    @doctor_count = @users_count.where(role:'doctor').count
-    @nurse_count = @users_count.where(role:'doctor').count
-    @patient_count = @users_count.where(role:'patient').count
+    @users  = User.all 
+    @admin_count  = @users.where(admin: true).count
+    @doctor_count = @users.where(role: 'doctor').count
+    @nurse_count = @users.where(role: 'nurse').count
+    @patient_count = @users.where(role: 'patient').count
   end
 end
