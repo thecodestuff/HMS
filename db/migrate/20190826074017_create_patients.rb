b7f254837e4a40d3dc5a3a286388efb207a860c2
thecodestuff <mahendrachoudhary1156@gmail.com>
2019-08-26 13:23:13 +0530

    patients model and migration created

---
+++ db/migrate/20190826074017_create_patients.rb
@@ -0,0 +1,12 @@
+class CreatePatients < ActiveRecord::Migration[5.2]
+  def change
+    create_table :patients do |t|
+      t.references :user, index: true
+      t.string : patient_type
+      t.integer: ward_assigned
+      t.boolean: status
+
+      t.timestamps
+    end
+  end
+end
