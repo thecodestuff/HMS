6b454428d9c8b6e713a519619a636c87e6ea0cf5
thecodestuff <mahendrachoudhary1156@gmail.com>
2019-08-26 13:24:38 +0530

    minor error patched in patient migration

--- db/migrate/20190826074017_create_patients.rb
+++ db/migrate/20190826074017_create_patients.rb
@@ -2,9 +2,9 @@ class CreatePatients < ActiveRecord::Migration[5.2]
   def change
     create_table :patients do |t|
       t.references :user, index: true
-      t.string : patient_type
-      t.integer: ward_assigned
-      t.boolean: status
+      t.string  :patient_type
+      t.integer :ward_assigned
+      t.boolean :status
 
       t.timestamps
     end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_08_22_130404) do
+ActiveRecord::Schema.define(version: 2019_08_26_074017) do
+
+  create_table "patients", force: :cascade do |t|
+    t.integer "user_id"
+    t.string "patient_type"
+    t.integer "ward_assigned"
+    t.boolean "status"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_patients_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
