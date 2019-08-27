43d762862cacd16e97d90a9799c7300899b63eb7
thecodestuff <mahendrachoudhary1156@gmail.com>
2019-08-27 15:52:34 +0530

    minor intentation formating

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -1,51 +1,46 @@
 class Admin::UsersController < Admin::AdminController
-
   def index
-    @users = User.all 
+    @users = User.all
   end
 
-  def new 
+  def new
     @user = User.new
   end
 
   def create
     @user = User.new(user_params)
-    
     respond_to do |format|
       if @user.save
-        format.html{ redirect_to admin_users_path, notice: 'user created successfully' }
+        format.html { redirect_to admin_users_path, notice: 'user created successfully' }
       else
-        format.html{redirect_to admin_users_path, notice: "#{@user.errors.details}"}
+        format.html { redirect_to admin_users_path, notice: @user.errors.details.to_s }
       end
     end
   end
 
   def show
-    render html: "hello"
+    render html: 'hello'
   end
 
   def edit
-     @user = User.find(params[:id])
+    @user = User.find(params[:id])
   end
 
   def update
     @user = User.find(params[:id])
     if @user.update(user_params)
-      flash[:notice] = "user updated successfully..."
-      redirect_to admin_users_path 
+      flash[:notice] = 'user updated successfully...'
     else
-      flash[:alert]="#{@user.errors.details}"
-      redirect_to admin_users_path
+      flash[:alert] = @user.errors.details.to_s
     end
+    redirect_to admin_users_path
   end
 
-  def destroy 
-    #User.delete(params[:id]) if User.exists?(params[:id])
-    if User.exists?(params[:id])
-      User.delete(params[:id])
-      flash[:notice] = "user deleted successfully..."
-      redirect_to admin_users_path 
-    end
+  def destroy
+    # User.delete(params[:id]) if User.exists?(params[:id])
+    User.delete(params[:id])
+    flash[:notice] = 'user deleted successfully...'
+    redirect_to admin_users_path
   end
 
   private
--- app/models/patient.rb
+++ app/models/patient.rb
@@ -1,5 +1,5 @@
 class Patient < ApplicationRecord
-  enum patient_type: [:outpatient, :inpatient, :emergency]
+  enum patient_type: %i[outpatient inpatient emergency]
   belongs_to :user
   has_many :appointments
   has_many :physicians, through: :appointments 
