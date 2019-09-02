fc15a013355544d7aa61d489f830451b762e518a
thecodestuff <mahendrachoudhary1156@gmail.com>
2019-09-02 19:35:09 +0530

    added date validation , appointment date cannot be in past

--- app/models/appointment.rb
+++ app/models/appointment.rb
@@ -2,6 +2,13 @@ class Appointment < ApplicationRecord
   enum patient_type: %i[pending done]
   belongs_to :physician
   belongs_to :patient
-  scope :today_appointment, ->(id){ where(physician_id: id, appointment_date: Date.current) }
-  scope :pending_appointment, ->(id){ where(status: 0, physician_id: id, appointment_date: Date.current) }
+  scope :today_appointment, ->(id) { where(physician_id: id, appointment_date: Date.current) }
+  scope :pending_appointment, ->(id) { where(status: 0, physician_id: id, appointment_date: Date.current) }
+
+  validates :physician_id, :patient_id, :appointment_date , presence:true
+  validate :appointment_date_cannot_be_in_past
+
+  def appointment_date_cannot_be_in_past
+    errors.add(:appointment_date, 'appointment date cannot be in past') unless appointment_date.day >= Date.current.day
+  end
 end
