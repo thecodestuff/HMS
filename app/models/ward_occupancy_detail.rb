49b355afc918c0dca9065455db6c3d07125db057
thecodestuff <mahendrachoudhary1156@gmail.com>
2019-09-02 14:36:39 +0530

    added empty ward select box to admit patient and handled the string error coming previously

--- app/models/ward_occupancy_detail.rb
+++ app/models/ward_occupancy_detail.rb
@@ -2,5 +2,5 @@ class WardOccupancyDetail < ApplicationRecord
   enum status: %i[EMPTY NOTEMPTY]
   enum ward_type: %i[GEN DEX GOLD]
   scope :wards, -> { count }
-  scope :status, -> { where(status: 0) }
+  scope :empty, -> { where(status: 0) }
 end
