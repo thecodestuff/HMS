604be4b69e411079f160d98d5b27aa414d8e264a
thecodestuff <mahendrachoudhary1156@gmail.com>
2019-09-10 11:05:33 +0530

    uniqnesss for ward name applied on ward occupancy model

--- app/models/ward_occupancy_detail.rb
+++ app/models/ward_occupancy_detail.rb
@@ -1,9 +1,10 @@
 class WardOccupancyDetail < ApplicationRecord
   enum status: %i[EMPTY NOTEMPTY]
   enum ward_type: %i[GEN DEX GOLD]
-  
+
   validates :ward_name, :ward_type, presence: true
-  
+  validates_uniqueness_of :ward_name
+
   scope :wards, -> { count }
   scope :empty, -> { where(status: 0) }
 end
