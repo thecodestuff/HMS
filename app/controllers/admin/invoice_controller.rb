3cef26899ae61c3066ee3cc8b94e0d2a744505d8
thecodestuff <mahendrachoudhary1156@gmail.com>
2019-09-27 12:55:44 +0530

    made concern for invoice_controller, edit profie button to user and a dropdown for dashboard

--- app/controllers/admin/invoice_controller.rb
+++ app/controllers/admin/invoice_controller.rb
@@ -3,6 +3,8 @@
 module Admin
   # create a invoice for patient
   class InvoiceController < ApplicationController
+    include Calculatable
+
     before_action :find_patient, only: [:create]
     before_action :invoice, only: [:create]
     before_action :find_invoice, only: %i[update_status show]
@@ -65,32 +67,32 @@ module Admin
       end
     end
 
-    def calculate_bill(patient)
-      calculate_days(patient) * ward_rate(patient) + calculate_appointments_charges(patient)
-    end
+    # def calculate_bill(patient)
+    #   calculate_days(patient) * ward_rate(patient) + calculate_appointments_charges(patient)
+    # end
 
-    def calculate_days(patient)
-      days = (patient.dischagre_on.day - patient.admit_date.day).abs
-      days.zero? ? 1 : days
-    end
+    # def calculate_days(patient)
+    #   days = (patient.dischagre_on.day - patient.admit_date.day).abs
+    #   days.zero? ? 1 : days
+    # end
 
-    def calculate_appointments(patient)
-      patient.appointments.where(status: 'done').count
-    end
+    # def calculate_appointments(patient)
+    #   patient.appointments.where(status: 'done').count
+    # end
 
-    def calculate_appointments_charges(patient)
-      appointments = patient.appointments.done
-                            .joins(:physician)
-                            .select(:appointment_date, :charge, :charge_at_weekend)
+    # def calculate_appointments_charges(patient)
+    #   appointments = patient.appointments.done
+    #                         .joins(:physician)
+    #                         .select(:appointment_date, :charge, :charge_at_weekend)
 
-      appointments.collect do |attr|
-        attr.appointment_date.on_weekend? ? attr.charge_at_weekend.to_i : attr.charge.to_i
-      end.sum
-    end
+    #   appointments.collect do |attr|
+    #     attr.appointment_date.on_weekend? ? attr.charge_at_weekend.to_i : attr.charge.to_i
+    #   end.sum
+    # end
 
-    def ward_rate(patient)
-      Invoice.rate[patient.ward_occupancy_detail.ward_type.to_sym]
-    end
+    # def ward_rate(patient)
+    #   Invoice.rate[patient.ward_occupancy_detail.ward_type.to_sym]
+    # end
 
     def find_invoice
       @patient_invoice = Invoice.find(params[:id])
---
+++ app/controllers/concerns/calculatable.rb
@@ -0,0 +1,32 @@
+# Use to calculate different billing entites that are required to 
+# to create invoice
+
+module Calculatable
+  extend ActiveSupport::Concern
+  def calculate_bill(patient)
+    calculate_days(patient) * ward_rate(patient) + calculate_appointments_charges(patient)
+  end
+
+  def calculate_days(patient)
+    days = (patient.dischagre_on.day - patient.admit_date.day).abs
+    days.zero? ? 1 : days
+  end
+
+  def calculate_appointments(patient)
+    patient.appointments.where(status: 'done').count
+  end
+
+  def calculate_appointments_charges(patient)
+    appointments = patient.appointments.done
+                          .joins(:physician)
+                          .select(:appointment_date, :charge, :charge_at_weekend)
+
+    appointments.collect do |attr|
+      attr.appointment_date.on_weekend? ? attr.charge_at_weekend.to_i : attr.charge.to_i
+    end.sum
+  end
+
+  def ward_rate(patient)
+    Invoice.rate[patient.ward_occupancy_detail.ward_type.to_sym]
+  end
+end
