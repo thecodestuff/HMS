ae18425d4935c473b1c4a0a506a54a3dcd705dfb
thecodestuff <mahendrachoudhary1156@gmail.com>
2019-10-09 17:27:19 +0530

    patch# migration change to add charge field into physician table

--- db/migrate/20190925110441_change_column_data_type_of_physicians.rb
+++ db/migrate/20190925110441_change_column_data_type_of_physicians.rb
@@ -1,5 +1,9 @@
 class ChangeColumnDataTypeOfPhysicians < ActiveRecord::Migration[5.2]
   def change
-    change_column :physicians, :charge, :decimal, using: 'charge::decimal', default: 100.00
+    # for develoment
+    #change_column :physicians, :charge, :decimal, using: 'charge::decimal', default: 100.00
+    
+    # production fix , revmove this in development
+    add_column :physicians, :charge, :decimal, using: 'charge::decimal', default: 100.00
   end
 end
