b6b918877b1cc0b10aed684c86862b43c7212faf
thecodestuff <mahendrachoudhary1156@gmail.com>
2019-10-09 16:51:00 +0530

    patch#1 to migration changing physician table column to decimal

--- db/migrate/20190925110441_change_column_data_type_of_physicians.rb
+++ db/migrate/20190925110441_change_column_data_type_of_physicians.rb
@@ -1,5 +1,5 @@
 class ChangeColumnDataTypeOfPhysicians < ActiveRecord::Migration[5.2]
   def change
-    change_column :physicians, :charge, :decimal, default: 100.00
+    change_column :physicians, :charge, :decimal, using: 'charge::decimal', default: 100.00
   end
 end
