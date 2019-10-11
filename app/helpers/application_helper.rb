0046a2e4d92f2b2f51e66edb42b67acf478e760c
thecodestuff <mahendrachoudhary1156@gmail.com>
2019-10-11 17:11:29 +0530

    active menu and crumboard added

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,4 +1,5 @@
 module ApplicationHelper
+  include Rails.application.routes.url_helpers
   # Displays object errors
   def form_errors_for(object = nil)
     render('shared/form_errors', object: object) unless object.blank?
@@ -8,7 +9,11 @@ module ApplicationHelper
     date.strftime('%d %B %Y') if date.present?
   end
 
-  def active_nav_link(path)
-    class_name = current_page?(path) ? 'current active' : ''
+  def active_nav_link(path, optional=nil)
+    current_page?(path) ? 'current active' : ''
+  end
+
+  def filter_original_path
+    request.original_fullpath.split("/").last.humanize
   end
 end
